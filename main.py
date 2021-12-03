d95d2c2c6d9a8ec6b5f617552f6600443dadcb82
GuilleIllana <guillermo19982012@gmail.com>
2021-12-03 12:48:18 +0100

    Adición de función que controla el cursor

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from PIL import ImageGrab
-import win32gui
 from time import sleep
+import win32gui, win32api, win32con
+import sys
 import subprocess
+import ctypes
 import cv2
 import numpy as np
 
 toplist, winlist = [], []
-
+SCREEN_WIDTH, SCREEN_HEIGHT = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
 
 def loadWindowsList(hwnd, toplist):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
 
-def loadTemplate(paths):
+def loadTemplate(paths):  # This function load and modify the template images
     template = []
     for path in paths:
         image = cv2.imread(path)
@@ -22,8 +24,7 @@ def loadTemplate(paths):
     return template
 
 
-def loadBoard(name):
-    # Capturing the Minesweeper screen
+def loadBoard(name):  # This function capture and crop the Minesweeper screen
     image = windowCapture(name)
 
     # Convert PIL to OpenCV
@@ -33,45 +34,45 @@ def loadBoard(name):
     return board_cropped
 
 
-def windowCapture(name):
-    # This function return an image of the Minesweeper tab
+def getHwnd(name):  # This function obtains the hwnd of the window with the provided name
+    win32gui.EnumWindows(loadWindowsList, toplist)
+    window = [(hwnd, title) for hwnd, title in winlist if name in title.lower()]
+    return window[0][0]
+
+
+def windowCapture(name):  # This function returns an image of the provided window name, in this case, Minesweeper
     try:
-        win32gui.EnumWindows(loadWindowsList, toplist)
-        window = [(hwnd, title) for hwnd, title in winlist if name in title.lower()]
-
-        hwnd = window[0][0]
-
+        hwnd = getHwnd(name)
         win32gui.SetForegroundWindow(hwnd)
         bbox = win32gui.GetWindowRect(hwnd)
         img = ImageGrab.grab(bbox)
         return img
 
-    # If Minesweeper can't be found, this code executes it
     except:
-        subprocess.Popen(r".\Buscaminas.exe")
-        sleep(1)
+        # If Minesweeper can't be found, this code executes it
+        print('Minesweeper not found. Executing Minesweeper...')
+        try:
+            subprocess.Popen(r".\Buscaminas.exe")
+            sleep(1)
 
-        win32gui.EnumWindows(loadWindowsList, toplist)
-        window = [(hwnd, title) for hwnd, title in winlist if name in title.lower()]
+            hwnd = getHwnd(name)
+            win32gui.SetForegroundWindow(hwnd)
+            bbox = win32gui.GetWindowRect(hwnd)
+            img = ImageGrab.grab(bbox)
+            return img
 
-        hwnd = window[0][0]
-
-        win32gui.SetForegroundWindow(hwnd)
-        bbox = win32gui.GetWindowRect(hwnd)
-        img = ImageGrab.grab(bbox)
-        return img
+        except:
+            sys.exit("Minesweeper couldn't be open")
 
 
-def boxCheck(image,template):
-
-    # Checks the board searching for known figures
+def boxCheck(image, template):  # This function checks the board searching for known figures
     for i in range(len(template)):
         if not(np.bitwise_xor(image, template[i]).any()):
             return i
     return None
 
 
-def obtainMatrix(board, template):
+def obtainMatrix(board, template):  # This function returns a matrix reflecting the state of the game
     # CV treatment of the image
     gray = cv2.cvtColor(board, cv2.COLOR_BGR2GRAY)
     (t, thresh) = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
@@ -95,23 +96,53 @@ def obtainMatrix(board, template):
     return board_mat
 
 
+def getPosition(hwnd, extra):  # This function returns the position of the window with the provided hwnd
+    rect = win32gui.GetWindowRect(hwnd)
+    x1 = rect[0]
+    y1 = rect[1]
+    x2 = rect[2]
+    y2 = rect[3]
+    return x1, x2, y1, y2
+
+
+def mouse(x, y, action=False):  # This function controls the mouse (action is usually disabled for debugging)
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE, int(x / SCREEN_WIDTH * 65535.0),
+                         int(y / SCREEN_HEIGHT * 65535.0))
+    if action:
+        # Click of the mouse
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+
 def main():
     # Path of the template images
     paths = []
-    paths.append(r'.\data\0.png')
-    paths.append(r'.\data\1.png')
-    paths.append(r'.\data\2.png')
-    paths.append(r'.\data\3.png')
-    paths.append(r'.\data\4.png')
-    paths.append(r'.\data\5.png')
-    paths.append(r'.\data\6.png')
-    paths.append(r'.\data\7.png')
-    paths.append(r'.\data\8.png')
-    paths.append(r'.\data\block.png')
-    paths.append(r'.\data\flag.png')
-    paths.append(r'.\data\bomb.png')
-    paths.append(r'.\data\bomb_2.png')
-    paths.append(r'.\data\bomb_3.png')
+
+    # Number of bombs surrounding the box
+    paths.append(r'.\data\0.png')  # 0
+    paths.append(r'.\data\1.png')  # 1
+    paths.append(r'.\data\2.png')  # 2
+    paths.append(r'.\data\3.png')  # 3
+    paths.append(r'.\data\4.png')  # 4
+    paths.append(r'.\data\5.png')  # 5
+    paths.append(r'.\data\6.png')  # 6
+    paths.append(r'.\data\7.png')  # 7
+    paths.append(r'.\data\8.png')  # 8
+
+    # Unknown block
+    paths.append(r'.\data\block.png')  # 9
+
+    # Flag
+    paths.append(r'.\data\flag.png')  # 10
+
+    # Bomb
+    paths.append(r'.\data\bomb.png')  # 11
+
+    # Bomb with flag on top
+    paths.append(r'.\data\bomb_2.png')  # 12
+
+    # Bomb which has exploded
+    paths.append(r'.\data\bomb_3.png')  # 13
 
     # Loading of all the templates
     template = loadTemplate(paths)
@@ -119,9 +150,21 @@ def main():
     # Loading of the Minesweeper board
     board = loadBoard('buscaminas')
 
-    # Obtain the matrix which represents the state of the game
+    # Obtaining the matrix which represents the state of the game
     game_mat = obtainMatrix(board, template)
     print(game_mat)
+
+    # Obtaining the position of the Minesweeper window
+    x1, _, y1, _ = getPosition(getHwnd('buscaminas'), None)
+
+    # Coordinates of the box to click
+    x = 10
+    y = 10
+
+    # Moving the mouse to a certain box
+    """Creo que esto solo vale para la resolución de mi pantalla (1440p) y no sería preciso con pantallas FullHD,
+    pero no estoy seguro"""
+    mouse(x1+22+16*x, y1+108+16*y) # (x,y)
 
 if __name__ == "__main__":
     main()
