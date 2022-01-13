9c0ece366d9ce1767d72f1967cdfb0db77ea609f
Pablo García Peris <pablogperis@gmail.com>
2022-01-13 20:29:30 +0100

    Va mal pero mi hermano me amenaza con borrarlo
    Así que lo subo

--- MS_eq_solver.py
+++ MS_eq_solver.py
@@ -1,14 +1,16 @@
 import Board_lib
 import numpy as np
 
-def expanded_board(Board): #cutre, cutre
+# Minesweeper equation system solver via model checking
+
+def expanded_board(Board): #para ponerle un "borde" al tablero (se amplían las dimensiones). Es cutre lo sé
     m = []
     r = []
     for j in range(Board.cols + 2):
         r.append(20) # borde
     m.append(r)
 
-    for i in range(Board.rows):
+    for i in range(Board.rows): # copia el tablero board, básicamente
         r = [20]
         for j in range(Board.cols):
             r.append(Board.cell[i][j].state)
@@ -23,6 +25,9 @@ def expanded_board(Board): #cutre, cutre
     return m
 
 def new_possible_solution(oa):
+    # a partir de un vector binario de 0/False (no mina) y 1/True (mina((False y True), donde este vector simboliza un
+    # posible modelo del mundo (model checking), se genera otro vector. Funciona básicamente como un sumador binario en 
+    # cadena
     if len(oa) > 1:
         na, c = new_possible_solution(oa[:-1])
     else:
@@ -33,9 +38,10 @@ def new_possible_solution(oa):
         return na, False
     else: # change
         na.append(not oa[-1])
-        return na, oa[-1] # carry-out = 1 if oa[end] was 1
+        return na, oa[-1] # carry out = 1 if oa[end] was 1
 
 def check_value(ec_sys, value):
+    # comprueba si el valor value se encuentra en todos los vectores de soluciones válidos, y devuelve la posición de estos
     s = []
     for j in range(len(ec_sys[0])):
         for i in range(len(ec_sys)):
@@ -45,7 +51,9 @@ def check_value(ec_sys, value):
                 s.append(j)
     return s
 
-def uncompress_1(n, row, col):
+# --- 1 ECUACIÓN ---
+
+def get_coords_1(n, row, col):# transforma una solución en las coordenadas del tablero (1 ecuación)
     k = 0
     for i in range(3):
         for j in range(3):
@@ -53,55 +61,183 @@ def uncompress_1(n, row, col):
                 continue
             elif n == k:
                 return row+i, col+j
-            k = k + 1
+            k += 1
     return -1
 
-
-def solve_system_1(eb, row, col):
-    n1 = eb[row+1][col+1]
-    candidate_simp_sys = []
-    inc = []
+def solve_system_1(eb, row, col): #resuelve el sistema de ecuaciones (en realidad, solo una ecuación) de una casilla
+    n1 = eb[row + 1][col + 1] #  (término independiente de la ecuación 1) # valor de la casilla central (término independiente de la ecuación)
+    candidate_solutions = [] # soluciones candidatas
+    inc = [] #incógnitas (vector que representa las posiciones de las casillas colindadntes sin descubrir)
 
     k = 0
     for i in range(3):
         for j in range(3):
             if i == 1 and j == 1:
                 continue
-            elif eb[row+i][col+j] == 9: #unknown
+            elif eb[row+i][col+j] == 9: #unknown, not discovered
                 inc.append(k) # se añade a la lista de incógnitas
-                candidate_simp_sys.append(False)
+                candidate_solutions.append(False) # el vector de soluciones candidatas empieza inicializado en ceros
             elif eb[row+i][col+j] == 10: #flag
                 n1 = n1 - 1 # se le resta 1 al valor de la suma
-            k = k+1
+            k += 1
+
+    if len(inc) == 0:
+        return -1, -1, -1  #no hay ninguna incógnita alrededor
+
+    valid_solutions = []
+    # todas las posibilidades
+    for i in range(2**len(inc)):
+        candidate_solutions, basura = new_possible_solution(candidate_solutions)
+        if sum(candidate_solutions) == n1:
+            valid_solutions.append(candidate_solutions)
+
+    if len(valid_solutions) == 0: #no hay un solo valor del sistema que cumpla las ecuaciones 
+        # (lo cual no sé muy bien por qué ocurre)
+        return -1, -1, -1
+
+    v = check_value(valid_solutions, 0)  # 0 = no mina -> pulsar
+    if len(v) != 0:
+        rn, cn = get_coords_1(inc[v[0]], row, col)
+        return rn, cn, 0 #row, column, no mine
+
+    v = check_value(valid_solutions, 1) #1 = mina
+    if len(v) != 0:
+        rm, cm = get_coords_1(inc[v[0]], row, col)
+        return rm, cm, 1 #row, column, mine
+    else:
+        return -1, -1, -1
+
+# --- 2 ECUACIONES VERTICAL ---
+
+def get_coords_2v(n, row, col):# transforma una solución en las coordenadas del tablero (2 ecuaciones, vertical)
+    k = 0
+    for i in range(4):
+        for j in range(3):
+            if (i == 1 or i == 2) and j == 1:
+                continue
+            elif n == k:
+                return row + i, col + j
+            k += 1
+    return -1
+
+def solve_system_2v(eb, row, col):
+
+    n1 = eb[row + 1][col + 1] # (término independiente de la ecuación 1)
+    n2 = eb[row + 2][col + 1] # (término independiente de la ecuación 2)
+    candidate_solutions = [] # soluciones candidatas
+    inc = [] #incógnitas (vector que representa las posiciones de las casillas colindadntes sin descubrir)
+
+    # ec. 1 -> filas (i) 0 a 2
+    # ec. 2 -> filas (i) 1 a 3
+    k = 0
+    for i in range(4):
+        for j in range(3):
+            if (i == 1 or i == 2) and j == 1:
+                continue
+            elif i == 1 and j == 0:
+                start2 = len(inc) # marca que indica desde qué parte del vector se corresponde con la ecuación 2
+            elif i == 3 and j == 0:
+                end1 = len(inc) # marca que indica hasta qué parte del vector se corresponde con la ecuación 1
+            if eb[row+i][col+j] == 9: #unknown
+                inc.append(k) # se añade a la lista de incógnitas
+                candidate_solutions.append(False)
+            elif eb[row+i][col+j] == 10: #flag
+                if i <= 2:
+                    n1 = n1 - 1 # se le resta 1 al valor de la suma (ec 1)
+                if i >= 1:
+                    n2 = n2 - 1 # se le resta 1 al valor de la suma (ec 2)
+            k += 1
 
     if len(inc) == 0:
         return -1, -1, -1 #no hay ninguna incógnita alrededor
 
-
-    valid_simp_sys = []
+    valid_solutions = []
     # todas las posibilidades
     for i in range(2**len(inc)):
-        candidate_simp_sys, basura = new_possible_solution(candidate_simp_sys)
-        if sum(candidate_simp_sys) == n1:
-            valid_simp_sys.append(candidate_simp_sys)
+        candidate_solutions, basura = new_possible_solution(candidate_solutions)
+        if sum(candidate_solutions[:end1]) == n1 and sum(candidate_solutions[start2:]) == n2:
+            valid_solutions.append(candidate_solutions)
 
-    if len(valid_simp_sys) == 0:
+    if len(valid_solutions) == 0:
         return -1, -1, -1
-    v = check_value(valid_simp_sys, 1) #1 = mina -> POSIBLE CAMBIO DE ORDEN MAAAAN
+    v = check_value(valid_solutions, 1) #1 = mina -> POSIBLE CAMBIO DE ORDEN MAAAAN
     if len(v) != 0:
-        rm, cm = uncompress_1(inc[v[0]], row, col)
+        rm, cm = get_coords_2v(inc[v[0]], row, col)
         #eb[rm][cm] = 10
         return rm, cm, 1
 
-    v = check_value(valid_simp_sys, 0)  # 0 = no mina -> pulsar
+    v = check_value(valid_solutions, 0)  # 0 = no mina -> pulsar
     if len(v) != 0:
-        rn, cn = uncompress_1(inc[v[0]], row, col)
+        rn, cn = get_coords_2v(inc[v[0]], row, col)
         #eb[rn][cn] = 15 ## prueba
         return rn, cn, 0
     else:
         return -1, -1, -1
 
+# --- 2 ECUACIONES HORIZONTAL ---
 
+def get_coords_2h(n, row, col):# transforma una solución en las coordenadas del tablero (2 ecuaciones, vertical)
+    k = 0
+    for j in range(4):
+        for i in range(3):
+            if (j == 1 or j == 2) and i == 1:
+                continue
+            elif n == k:
+                return row + i, col + j
+            k += 1
+    return -1
 
+def solve_system_2h(eb, row, col):
 
+    n1 = eb[row + 1][col + 1] # (término independiente de la ecuación 1)
+    n2 = eb[row + 2][col + 1] # (término independiente de la ecuación 2)
+    candidate_solutions = [] # soluciones candidatas
+    inc = [] #incógnitas (vector que representa las posiciones de las casillas colindadntes sin descubrir)
+
+    # ec. 1 -> columnas (j) 0 a 2
+    # ec. 2 -> columnas (j) 1 a 3
+    k = 0
+    for j in range(4):
+        for i in range(3):
+            if (j == 1 or j == 2) and i == 1:
+                continue
+            elif j == 1 and i == 0:
+                start2 = len(inc) # marca que indica desde qué parte del vector se corresponde con la ecuación 2
+            elif j == 3 and i == 0:
+                end1 = len(inc) # marca que indica hasta qué parte del vector se corresponde con la ecuación 1
+            if eb[row+i][col+j] == 9: #unknown
+                inc.append(k) # se añade a la lista de incógnitas
+                candidate_solutions.append(False)
+            elif eb[row+i][col+j] == 10: #flag
+                if j <= 2:
+                    n1 = n1 - 1 # se le resta 1 al valor de la suma (ec 1)
+                if j >= 1:
+                    n2 = n2 - 1 # se le resta 1 al valor de la suma (ec 2)
+            k += 1
+
+    if len(inc) == 0:
+        return -1, -1, -1 #no hay ninguna incógnita alrededor
+
+    valid_solutions = []
+    # todas las posibilidades
+    for i in range(2**len(inc)):
+        candidate_solutions, basura = new_possible_solution(candidate_solutions)
+        if sum(candidate_solutions[:end1]) == n1 and sum(candidate_solutions[start2:]) == n2:
+            valid_solutions.append(candidate_solutions)
+
+    if len(valid_solutions) == 0:
+        return -1, -1, -1
+    v = check_value(valid_solutions, 1) #1 = mina -> POSIBLE CAMBIO DE ORDEN MAAAAN
+    if len(v) != 0:
+        rm, cm = get_coords_2h(inc[v[0]], row, col)
+        #eb[rm][cm] = 10
+        return rm, cm, 1
+
+    v = check_value(valid_solutions, 0)  # 0 = no mina -> pulsar
+    if len(v) != 0:
+        rn, cn = get_coords_2h(inc[v[0]], row, col)
+        #eb[rn][cn] = 15 ## prueba
+        return rn, cn, 0
+    else:
+        return -1, -1, -1
 
--- main.py
+++ main.py
@@ -22,9 +22,38 @@ def main():
     resultados = []
     while partidas < n_partidas:
 
-        #wcf.click_board(-1, -1)
-        #board.show_board()
+        # PARA PAUSAR PULSAR EL F6
+        state = win32api.GetKeyState(117)
+        while state:
+            state = win32api.GetKeyState(117)
+        gc.collect()
 
+
+        # if it loses restart the game
+        if board.check_dead():
+            print("Perdiste!")
+            print("--- Perdiste en %s segundos ---" % (time.time() - start_time))
+            print("\n")
+            board.show_board()
+
+            resultados.append(0)
+            partidas = partidas + 1
+            board = wcf.init_game()
+            board.new_game()
+            start_time = time.time()
+
+        if board.check_solved():
+            print("Has ganado")
+            print("--- GANASTE en %s segundos ---" % (time.time() - start_time))
+            print("\n")
+
+            board.show_board()
+
+            resultados.append(1)
+            partidas = partidas + 1
+            board = wcf.init_game()
+            board.new_game()
+            start_time = time.time()
 
         '''while True:
             try:
@@ -43,74 +72,86 @@ def main():
         # print(board.next_move())
         mat = np.array(mses.expanded_board(board))
 
-        #print('mat')
         move = 0
+
+        # sistema de 1 ecuación
         i = 0
         j = 0
-        #print(board.rows)
-        #print(board.cols)
         while i < board.rows and move == 0:
             j = 0
             while j < board.cols and move == 0:
-                #print(i, j)
-                if mat[i + 1][j + 1] == 0 or mat[i + 1][j + 1] == 9 or mat[i + 1][j + 1] == 10 or mat[i + 1][
-                    j + 1] == 15:
+                if mat[i + 1][j + 1] == 0 or mat[i + 1][j + 1] == 9 or mat[i + 1][j + 1] == 10:
                     j = j + 1
                     continue
                 r, c, m = mses.solve_system_1(mat, i, j)
                 if m == 0:
                     wcf.click_board(c - 1, r - 1)
-                    #time.sleep(0.2)
-                    #print('no mina', r - 1, c - 1)
                     move = 1
+                    print('1')
                 elif m == 1:
                     wcf.click_board_right(c - 1, r - 1)
-                    #time.sleep(0.2)
-                    #print('mina', r - 1, c - 1)
                     move = 1
-                j = j + 1
-            i = i + 1
+                    print('1')
+                j += 1
+            i += 1
 
-        if move == 0:
-            i, j = board.next_move()
-            wcf.click_board(j, i)
+        if move == 1:
+            continue
 
-        # if it looses restart the game
-        if board.check_dead():
-            print("Perdiste!")
-            print("--- Perdiste en %s segundos ---" % (time.time() - start_time))
-            print("\n")
-            board.show_board()
+        # sistema de 2 ecuaciones (vertical)
+        i = 0
+        j = 0
+        while i < board.rows and move == 0:
+            j = 0
+            while j < board.cols - 1 and move == 0:
+                if mat[i + 1][j + 1] == 0 or mat[i + 1][j + 1] == 9 or mat[i + 1][j + 1] == 10 or mat[i + 2][j + 1] == 0 or mat[i + 2][j + 1] == 9 or mat[i + 1][j + 1] == 10:
+                    j = j + 1
+                    continue
+                r, c, m = mses.solve_system_2v(mat, i, j)
+                if m == 0:
+                    wcf.click_board(c - 1, r - 1)
+                    move = 1
+                    print('2v')
+                elif m == 1:
+                    wcf.click_board_right(c - 1, r - 1)
+                    move = 1
+                    print('2v')
+                j += 1
+            i += 1
 
-            resultados.append(0)
-            partidas = partidas + 1
-            board = wcf.init_game()
-            board.new_game()
-            start_time = time.time()
+        if move == 1:
+            continue
+
+        # sistema de 2 ecuaciones (horizontal)
+        i = 0
+        j = 0
+        while i < board.rows - 1 and move == 0:
+            j = 0
+            while j < board.cols and move == 0:
+                if mat[i + 1][j + 1] == 0 or mat[i + 1][j + 1] == 9 or mat[i + 1][j + 1] == 10 or mat[i + 1][j + 2] == 0 or mat[i + 1][j + 2] == 9 or mat[i + 1][j + 2] == 10:
+                    j = j + 1
+                    continue
+                r, c, m = mses.solve_system_2h(mat, i, j)
+                if m == 0:
+                    wcf.click_board(c - 1, r - 1)
+                    move = 1
+                    print('2h')
+                elif m == 1:
+                    wcf.click_board_right(c - 1, r - 1)
+                    move = 1
+                    print('2h')
+                j += 1
+            i += 1
+
+        if move == 1:
+            continue
+
+        # si no ha encontrado ningún sistema resoluble, usa el heurístico
+        i, j = board.next_move()
+        wcf.click_board(j, i)
+        print('h')
 
 
-            #wcf.click_board(5, 5)
-
-        if board.check_solved():
-            print("Has ganado")
-            print("--- GANASTE en %s segundos ---" % (time.time() - start_time))
-            print("\n")
-            
-            board.show_board()
-
-            resultados.append(1)
-            partidas = partidas + 1
-            board = wcf.init_game()
-            board.new_game()
-            start_time = time.time()
-
-
-
-        # PARA PAUSAR PULSAR EL F6
-        state = win32api.GetKeyState(117)
-        while state:
-            state = win32api.GetKeyState(117)
-        gc.collect()
 
     ganadas = (resultados.count(1)/n_partidas)*100
     perdidas = (resultados.count(0)/n_partidas)*100
